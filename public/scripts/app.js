710d5e70ccb820a7a2f98b98ab9c962f4b7269bc
fuzzylogicxx <laurence@fuzzylogicdigital.com>
2019-09-12 14:19:46 +0100

    yarn init for dependency management; and installed babel-preset-react and babel-preset-env

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,7 +2,6 @@ console.log('App.js is running');
 
 //var template = <p>This is JSX from app.js</p>;
 var template = React.createElement("p", null, "This is JSX from app.js");
-
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
