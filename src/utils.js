713b6459202de1acff55d34b5097fea546934432
fuzzylogicxx <laurence@fuzzylogicdigital.com>
2019-09-25 12:36:37 +0100

    Moved components into own files; app.js now just bootstraps the app

--- src/utils.js
+++
@@ -1,9 +0,0 @@
-console.log('utils.js is runnning');
-
-const square = (num) => num * num;
-
-const add = (a, b) => a + b;
-
-const subtract = (a, b) => a - b;
-
-export { square, add, subtract as default };
