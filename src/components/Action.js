0878e4384608b6ee4f8b7ffb20a3bb833395f575
fuzzylogicxx <laurence@fuzzylogicdigital.com>
2019-09-25 20:34:22 +0100

    Installed normalize.css via yarn; added some (BEM-based) CSS components

--- src/components/Action.js
+++ src/components/Action.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Action = (props) => (
     <div>
         <button
+            className="main-button" 
             onClick={props.handlePick} 
             disabled={!props.hasOptions}
         >
