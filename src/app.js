5f6862ca2c2fe800719996f0eb14c336e6d57ec9
fuzzylogicxx <laurence@fuzzylogicdigital.com>
2019-09-25 10:56:25 +0100

    Installed npm modules react, react-dom; babel-core, babel-loader; and configured webpack to load babel on JS files we wrote

--- src/app.js
+++ src/app.js
@@ -1,14 +1,5 @@
-//import './utils.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-//import subtract, { square, add } from './utils.js';
-
-import isSenior, { isAdult, canDrink } from './person.js'
-
-console.log('app.js is running');
-// console.log(square(4));
-// console.log(add(2, 5));
-// console.log(subtract(9, 5));
-
-console.log(isAdult(17));
-console.log(canDrink(24))
-console.log(isSenior(65))
+const template = <p>Test</p>;
+ReactDOM.render(template, document.getElementById('app'));
